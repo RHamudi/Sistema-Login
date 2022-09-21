bb6ff77612446efdd745bcf24bc31f17e8af607f
RHamudi <ramonramos.silva16@gmail.com>
2022-09-21 18:35:41 -0300

    Routes / Private

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { useContext } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Private } from "./pages/Private";
+import { RequireAuth } from "./contexts/Auth/RequireAuth";
+import { AuthContext } from "./contexts/Auth/AuthContext";
 
 function App() {
+  const auth = useContext(AuthContext);
+
+  async function handleLogout() {
+    auth.signout();
+    window.location.href = window.location.href;
+  }
+
   return (
     <div className="App">
       <header>
@@ -11,11 +21,19 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/private">Página Privada</Link>
+          {auth.user && <button onClick={handleLogout}>Sair</button>}
         </nav>
         <hr />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/private" element={<Private />} />
+          <Route
+            path="/private"
+            element={
+              <RequireAuth>
+                <Private />
+              </RequireAuth>
+            }
+          />
         </Routes>
       </header>
     </div>
